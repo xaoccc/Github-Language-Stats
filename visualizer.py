b48e9735b05af1449542f87bbaa9d37e41765e1d
xaoccc <xaocccc@gmail.com>
2026-02-24 15:07:17 +0200

    pie chart settings show % for lang > 1%

--- visualizer.py
+++ visualizer.py
@@ -545,7 +545,7 @@ class Visualizer:
         colors = [self._get_color(lang) if lang != 'Other' else '#d0d0d0' for lang in languages]
 
         def autopct_format(pct):
-            return f'{pct:.1f}%' if pct > 3 else ''
+            return f'{pct:.1f}%' if pct > 1 else ''
 
         wedges, texts, autotexts = ax.pie(
             values,
